441d9296a43b708105fb691b6907819ab62ff3e3
khisr0w <khisrow_hashimi@hotmail.com>
2025-08-18 11:56:43 +0200

    refactor

--- lua/utils.lua
+++ lua/utils.lua
@@ -1,67 +1,71 @@
-return {
-    font_size_step = function(font_str, offset)
-        start_pos = string.find(font_str, ":")
-        end_pos = string.find(font_str, ":", start_pos+1)
-        prefix = string.sub(font_str, 0, start_pos+1)
-        font_size = tonumber(string.sub(font_str, start_pos+2, end_pos-1)) + offset
-        suffix = string.sub(font_str, end_pos)
+local M = {}
 
-        vim.opt.guifont = prefix .. font_size ..suffix
-    end,
+M.font_size_step = function(font_str, offset)
+    start_pos = string.find(font_str, ":")
+    end_pos = string.find(font_str, ":", start_pos+1)
+    prefix = string.sub(font_str, 0, start_pos+1)
+    font_size = tonumber(string.sub(font_str, start_pos+2, end_pos-1)) + offset
+    suffix = string.sub(font_str, end_pos)
 
-    check_args = function(flag)
-        for _, arg in ipairs(vim.v.argv) do
-            if arg == flag then
-                return true
-            end
+    vim.opt.guifont = prefix .. font_size ..suffix
+end
+
+M.check_args = function(flag)
+    for _, arg in ipairs(vim.v.argv) do
+        if arg == flag then
+            return true
         end
-        return false
-    end,
-    run_command_async = function(command, args, callback)
-        local stdout = vim.loop.new_pipe(false)
-        local stderr = vim.loop.new_pipe(false)
-        local handle
+    end
+    return false
+end
 
-        local function on_exit(code, signal)
-            stdout:close()
-            stderr:close()
-            handle:close()
-            if callback then
-                callback(code, signal)
-            end
+M.run_command_async = function(command, args, callback)
+    local stdout = vim.loop.new_pipe(false)
+    local stderr = vim.loop.new_pipe(false)
+    local handle
+
+    local function on_exit(code, signal)
+        stdout:close()
+        stderr:close()
+        handle:close()
+        if callback then
+            callback(code, signal)
         end
+    end
 
-        handle = vim.loop.spawn(
-            command,
-            {
-                args = args,
-                stdio = {nil, stdout, stderr},
-            },
-            on_exit
-        )
+    handle = vim.loop.spawn(
+        command,
+        {
+            args = args,
+            stdio = {nil, stdout, stderr},
+        },
+        on_exit
+    )
 
-        vim.loop.read_start(stdout, vim.schedule_wrap(function(err, data)
-            -- if err then
-            --     print("Error reading stdout:", err)
-            -- elseif data then
-            --     print("stdout:", data)
-            -- end
-        end))
+    vim.loop.read_start(stdout, vim.schedule_wrap(function(err, data)
+        -- if err then
+        --     print("Error reading stdout:", err)
+        -- elseif data then
+        --     print("stdout:", data)
+        -- end
+    end))
 
-        vim.loop.read_start(stderr, vim.schedule_wrap(function(err, data)
-            if err then
-                print("Error reading stderr:", err)
-            elseif data then
-                print("stderr:", data)
-            end
-        end))
-    end,
-    string_split = function(str, delim)
-        local t = {}
-        for substr in str.gmatch(str, "([^" .. delim .. "]+)") do
-            table.insert(t, substr)
+    vim.loop.read_start(stderr, vim.schedule_wrap(function(err, data)
+        if err then
+            print("Error reading stderr:", err)
+        elseif data then
+            print("stderr:", data)
         end
+    end))
+end
 
-        return t
-    end,
-}
+M.string_split = function(str, delim)
+    local t = {}
+    for substr in str.gmatch(str, "([^" .. delim .. "]+)") do
+        table.insert(t, substr)
+    end
+
+    return t
+end
+
+return M
